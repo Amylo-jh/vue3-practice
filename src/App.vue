774bc869658d1adf9194e894ac70127f410ae58e
Amylo <amylose1110@gmail.com>
2023-12-07 21:58:37 +0900

    Part 9 - Ch 2 - 06. Watch

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,33 @@
 <template>
-  <button @click="add">
-    ADD
-  </button>
-  <h1>    {{ reversedMessage }}  </h1>
-  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
-  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
-  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
+  <h1 @click="changeMessage">
+    {{ msg }}
+  </h1>
+  <h1> {{ reversedMessage }}</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      // Getter, Setter
-      msg: 'Hello Computed!'
-    }
-  },
-  methods: {
-    // Setter
-    add() {
-      this.reverseddMessage += '!?'
+      msg : "Hello?"
     }
   },
   computed: {
-    // Getter
     reversedMessage() {
       return this.msg.split('').reverse().join('')
+    }
+  },
+  watch: {
+    msg(newValue) {
+      console.log(newValue)
     },
-    // Getter, Setter
-    reverseddMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      },
-      set(newValue) {
-        this.msg = newValue
-      }
+    reversedMessage() {
+      console.log('reversedMessage: ', this.reversedMessage)
+    }
+  },
+  methods: {
+    changeMessage() {
+      this.msg = "Good!"
     }
   }
-}
-</script>
+}</script>
