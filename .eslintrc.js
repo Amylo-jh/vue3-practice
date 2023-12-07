65d1905522b3535f98a4cde4bc1937cf9bc5b08e
Amylo <amylose1110@gmail.com>
2023-12-07 21:39:16 +0900

    Part 8 - Ch 2 - 04. Computed 캐싱

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        // vue
+        //'plugin:vue/vue3-essential', //lv 1
+        'plugin:vue/vue3-strongly-recommended', //lv2
+        //'plugin:vue/vue3-recommended' //lv3
+        // js
+        'eslint:recommended'
+    ],
+    rules: {
+        "vue/html-closing-bracket-newline" : ["error", {
+            "singleline" : "never",
+            "multiline" : "never"
+        }]
+    }
+}
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>    {{ reversedMessage }}  </h1>
+  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
+  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
+  <h1>    {{ msg.split('').reverse().join('') }}  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Computed!'
+    }
+  },
+  methods: {
+    reverseMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  computed: {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  }
+}
+</script>
