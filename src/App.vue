49e314472662d1b82e302e7b212c2547b7e5b429
Amylo <amylose1110@gmail.com>
2023-12-14 15:41:19 +0900

    Part 9 - Ch 2 - 11. 이벤트 핸들링 - 이벤트 수식어

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,48 @@
 <template>
-  <button @click="handler('hi'), handlerB()">
-    Click 1
-  </button>
-    <button @click="handler('what')">
-    Click 2
-  </button>
+  <a
+    href="https://naver.com"
+    target="_blank"
+    @click.prevent.once="handler">naver</a>
+
+  <div
+    class="parent"
+    @wheel.passive="handlerA">
+    <div
+      class="child" />
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
-    handler(msg) {
-      console.log(msg)
+    handler() {
+
+      console.log('ABC!')
+    },
+    handlerA(event) {
+      console.log(event)
+      console.log(event.target)
+      console.log(event.currentTarget)
+      console.log('A')
     },
     handlerB() {
       console.log('B')
     }
   }
+}</script>
+
+<style scoped lang="scss">
+.parent {
+  width: 200px;
+  height: 100px;
+  background-color: royalblue;
+  margin: 10px;
+  padding: 10px;
+  overflow: auto;
+  .child {
+    width: 100px;
+    height: 2000px;
+    background-color: orange;
+  }
 }
-</script>
+</style>
