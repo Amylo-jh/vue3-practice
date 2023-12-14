8b1d47c03879fed41e11654c0466e68e2bfbd421
Amylo <amylose1110@gmail.com>
2023-12-14 15:46:59 +0900

    Part 9 - Ch 2 - 12. 이벤트 핸들링 - 키 수식어

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,13 @@
 <template>
-  <a
-    href="https://naver.com"
-    target="_blank"
-    @click.prevent.once="handler">naver</a>
-
-  <div
-    class="parent"
-    @wheel.passive="handlerA">
-    <div
-      class="child" />
-  </div>
+  <input type="text"
+  @keydown.ctrl.shift.a="handler">
 </template>
 
-<script>
+<script> 
 export default {
   methods: {
-    handler() {
-
-      console.log('ABC!')
-    },
-    handlerA(event) {
+    handler(event) {
       console.log(event)
-      console.log(event.target)
-      console.log(event.currentTarget)
-      console.log('A')
-    },
-    handlerB() {
-      console.log('B')
     }
   }
 }</script>
-
-<style scoped lang="scss">
-.parent {
-  width: 200px;
-  height: 100px;
-  background-color: royalblue;
-  margin: 10px;
-  padding: 10px;
-  overflow: auto;
-  .child {
-    width: 100px;
-    height: 2000px;
-    background-color: orange;
-  }
-}
-</style>
