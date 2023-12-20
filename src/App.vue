cb4958fe68fce6c89f2361dd01f2729dbdbd7b0f
Amylo <amylose1110@gmail.com>
2023-12-20 21:45:05 +0900

    Part 9 - Ch 2 - 13. 이벤트 핸들링 - 폼 입력 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <template>
-  <input type="text"
-  @keydown.ctrl.shift.a="handler">
+  <h1> {{ msg }}</h1>
+  <input
+    type="text"
+    v-model="msg">
+  <h1>{{ checked }}</h1>
+  <input
+    type="checkbox"
+    name=""
+    id=""
+    v-model="checked">
 </template>
 
 <script> 
 export default {
+  data() {
+    return {
+      msg: 'hello world',
+      checked: false
+    }
+  },
   methods: {
     handler(event) {
-      console.log(event)
+      console.log(event.target.value)
+      this.msg = event.target.value
     }
   }
 }</script>
